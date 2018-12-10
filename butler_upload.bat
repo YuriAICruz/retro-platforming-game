1fced1a82e1a6530b2bac01fd67c7165cab0910b
Yuri Cruz <yuri.cruz@vldomain.vl.com>
2018-12-10 21:01:48 -0200

    minor update, butler upload file

---
+++ butler_upload.bat
@@ -0,0 +1,3 @@
+butler push Build/linux graphene-ai/hyperboy:linux --userversion 0.1
+butler push Build/mac graphene-ai/hyperboy:mac --userversion 0.1
+butler push Build/win graphene-ai/hyperboy:win --userversion 0.1
